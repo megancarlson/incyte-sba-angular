498a70dcd5f154bee2a205e3f2534afcd717d2b0
DavidBoboechie <114914307+DavidBoboechie@users.noreply.github.com>
2024-06-25 10:27:51 -0400

    Update query-intent.service.ts

--- projects/vanilla-search/src/app/query-intent.service.ts
+++ projects/vanilla-search/src/app/query-intent.service.ts
@@ -13,21 +13,39 @@ export class QueryIntentService {
     ){
         this.init();
     }
-
+  
     init() {
         this.searchService.events.subscribe(event => {
             if(event.type === 'new-query-intents') {
-                if(event.intents.find(intent => intent.name === 'codeDetectionQueryIntent'))
+                const incyteQueryIntent = event.intents.find(intent => intent.name === "codeDetectionQueryIntent");
+                if(incyteQueryIntent)
                 {
-                    if (this.processedIntents.indexOf('code' + this.searchService.query.text) < 0)
+                    // Option #1 - Disable neural search
+                    // if (this.processedIntents.indexOf('code' + this.searchService.query.text) < 0) 
+                    // {
+                    //     this.resetProcessedIntents();
+                    //     this.disableNeuralQI.next(true);
+                    //     this.processedIntents.push('code' + this.searchService.query.text);
+                    // }
+                    
+                    //Option #2 - Add search operators to filter passages
+                    if(this.searchService.query.text != null && this.searchService.query.text?.indexOf("+") === -1)
                     {
-                        this.resetProcessedIntents();
-                        this.disableNeuralQI.next(true);
-                        this.processedIntents.push('code' + this.searchService.query.text);
+                        event.intents.forEach(intent => {
+                            intent.globalEntities?.forEach(entity => {
+                                this.searchService.query.text = this.searchService.query.text?.replace(entity["surface"], "+(" + entity["surface"] + ")");
+
+                            })
+                        })
+                        this.searchService.search();
                     }
                 }
+                else
+                {
+                    // this.disableNeuralQI.next(false);
+                }
             }
-        })
+        });
     }
 
     public resetProcessedIntents() {
